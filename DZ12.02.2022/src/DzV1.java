a26f2e5c2c10fb90e7bc144d59afd88915e7184e
ZiEvgeniy <mzors@mail.ru>
2022-02-13 00:55:17 +0300

    Домашка 1

---
+++ DZ12.02.2022/src/DzV1.java
@@ -0,0 +1,30 @@
+public class DzV1 {
+    public static void main(String[] args) {
+
+        int a = -9;
+        if (a < 0) {
+            // Если a менше ноля то "Число отрицательное"
+            System.out.println("Число отрицательное");
+        } else if (a == 0) {
+            // Если а равно нулю...
+            System.out.println("Число равняется нулю");
+        } else {
+            // Если "a" больше ноля то "Число положительное"
+            System.out.println("Число положитлеьное");
+        }
+            if (-9 <= a) {
+                // Если a менше ноля то "Число отрицательное"
+                System.out.println("Ч+");
+            } else if (a == 0) {
+                // Если а равно нулю...
+                System.out.println("Число равняется нулю");
+            } else {
+                // Если "a" больше ноля то "Число положительное"
+                System.out.println("Число положитлеьное");
+            }
+                System.out.println("a++ = " + a++);
+                System.out.println("a++ = " + a);
+
+        // System.out.println("Программа закончена");
+    }
+}
