b985e7e98dff8e2e9df64344a03aa7abb182676f
ZiEvgeniy <mzors@mail.ru>
2022-02-13 11:12:09 +0300

    Решена первая задача, но без объединения в одну строку. Есть понимание логики, но как осуществить не понял.

--- DZ12.02.2022/src/DzV1.java
+++ DZ12.02.2022/src/DzV1.java
@@ -1,30 +1,48 @@
 public class DzV1 {
     public static void main(String[] args) {
 
-        int a = -9;
-        if (a < 0) {
-            // Если a менше ноля то "Число отрицательное"
-            System.out.println("Число отрицательное");
-        } else if (a == 0) {
-            // Если а равно нулю...
-            System.out.println("Число равняется нулю");
-        } else {
-            // Если "a" больше ноля то "Число положительное"
-            System.out.println("Число положитлеьное");
+        int a, b;
+        a = 0;
+        b = a;
+        if (a < 0)
+        {
+            // Если "a" менше ноля то...
+            System.out.println("это число отрицательное");
         }
-            if (-9 <= a) {
-                // Если a менше ноля то "Число отрицательное"
-                System.out.println("Ч+");
-            } else if (a == 0) {
-                // Если а равно нулю...
-                System.out.println("Число равняется нулю");
-            } else {
-                // Если "a" больше ноля то "Число положительное"
-                System.out.println("Число положитлеьное");
-            }
-                System.out.println("a++ = " + a++);
-                System.out.println("a++ = " + a);
+        else if (a == 0)
+        {
+            // Если "а" равно нулю...
+            System.out.println("это число ноль");
+        }
+        else
+        {
+            // Если "а" больше ноля...
+            System.out.println("это число положитлеьное");
+        }
+        if ( -9 <= b && b <= 9 )
+        {
+            System.out.println(" однозначное ");
+        }
+        else if ( -99 <= b && b <= -10 )
+        {
+            System.out.println(" двузначное ");
+        }
+        else if ( b >= 10 && b <= 99 )
+        {
+            System.out.println(" двузначное ");
+        }
+        else if ( -999 <= b && b <= -100 )
+        {
+            System.out.println(" трехзначное ");
+        }
+        else if ( b >= 100 && b <= 999 )
+        {
+            System.out.println(" трехзначное ");
+        }
 
-        // System.out.println("Программа закончена");
+         System.out.println("Программа закончена");
+
+
     }
+
 }
